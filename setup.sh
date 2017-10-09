6d5ecbf1e4f37cf12a97a1ab8d04311cda67e557
Baylac-Jacqué Félix <felix@alternativebit.fr>
2017-10-09 14:21:00 +0200

    Install neovim plug during setup.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 mkdir -p ~/.config/nvim/
 mkdir -p ~/.config/i3/
 
