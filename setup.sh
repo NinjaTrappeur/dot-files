9bd810c16ceae15175cd3cb683888bc3666cff20
Baylac-Jacqué Félix <felix@alternativebit.fr>
2017-10-10 16:02:12 +0200

    Fixed i3 config file path.

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim/
-mkdir -p ~/.config/i3/
+mkdir -p ~/.i3/
 
 cp init.vim ~/.config/nvim/init.vim
-cp i3/config ~/.config/i3/config
+cp i3/config ~/.i3/config
 cp .zshrc ~/.zshrc
